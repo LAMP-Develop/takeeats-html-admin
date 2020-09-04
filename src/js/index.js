f7fc59040e582a14250b423f8667cf281b1c3b0a
ShunyaSuzuki <shunya_suzuki@ShunyaSuzukinoMacBook-Air.local>
2020-09-04 14:34:07 +0900

    update

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,10 @@ import "../scss/app.scss";
 
 // JSインポート
 import "bootstrap";
-// import "./geo";
+
+$(function() {
+  $('li.parent').on('click', function () {
+    $(this).children('ul').slideToggle();
+    $(this).toggleClass('toggle-open');
+  });
+});
