5082a04236390bffad536de174eba0b288b6710b
ShunyaSuzuki <shunya_suzuki@ShunyaSuzukinoMacBook-Air.local>
2020-09-07 11:58:17 +0900

    top fixed

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,22 @@ import "../scss/app.scss";
 // JSインポート
 import "bootstrap";
 
-$(function() {
-  $('li.parent').on('click', function () {
-    $(this).children('ul').slideToggle();
-    $(this).toggleClass('toggle-open');
+$(function () {
+  // SPメニュー
+  $("#spmenuopen").on("click", function () {
+    $(this).toggleClass("open");
+    $("body").toggleClass("menuopen");
+    $("#spmenu").toggleClass("open");
+  });
+  $(".orverlay").on("click", function () {
+    $(this).toggleClass("open");
+    $("body").toggleClass("menuopen");
+    $("#spmenu").toggleClass("open");
+  });
+
+  // メニュートグル
+  $("li.parent").on("click", function () {
+    $(this).children("ul").slideToggle();
+    $(this).toggleClass("toggle-open");
   });
 });
