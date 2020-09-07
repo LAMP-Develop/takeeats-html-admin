1f247c8ee3de4faaa8b8621d31ff9f6eafdac372
ShunyaSuzuki <shunya_suzuki@ShunyaSuzukinoMacBook-Air.local>
2020-09-07 14:17:06 +0900

    update

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,16 @@ import "../scss/app.scss";
 import "bootstrap";
 
 $(function () {
+  // スムーススクロール
+  $('a[href^="#"]').on("click", function () {
+    let speed = 500;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+
   // SPメニュー
   $("#spmenuopen").on("click", function () {
     $(this).toggleClass("open");
